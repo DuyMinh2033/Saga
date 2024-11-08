3d1f67a25c3a4c1f013201bbf8e3670f13c831a2
DuyMinh2033 <tdminh334546@gmail.com>
2024-11-08 21:16:36 +0700

    MINH

--- src/Page/InputIOS/index.jsx
+++ src/Page/InputIOS/index.jsx
@@ -13,11 +13,7 @@ const InputIOS = () => {
 
   const onChangeValue = (event) => {
     const newValue = event.target.value;
-    // Kiểm tra nếu chỉ chứa số
-    if (/^\d*$/.test(newValue)) {
-      setValueLabel(newValue); // Cập nhật giá trị nếu là số
-    }
-    // Nếu nhập ký tự đặc biệt thì không thay đổi giá trị
+    setValueLabel(newValue);
   };
 
   useEffect(() => {
@@ -28,11 +24,10 @@ const InputIOS = () => {
     <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
       <input
         ref={Ref}
-        type="text" // Sử dụng type="text" thay vì type="number"
-        onKeyDown={handleKeyDown}
+        type="number"
         onChange={onChangeValue}
-        value={valueLabel} // Đảm bảo hiển thị giá trị hợp lệ
-        inputMode="numeric" // Chỉ cho phép hiển thị bàn phím số
+        onKeyDown={handleKeyDown}
+        inputMode="numeric"
       />
       <label style={{ color: "red" }}>
         {valueLabel !== "" ? valueLabel : "No value"}
