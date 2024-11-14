c02c7b81a6cd22df8c27db8848163c63fc30cf9c
Duy Minh <tdminh334546@gmail.com>
2024-11-14 10:57:27 +0700

    tesst

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/", // Đảm bảo base được đặt thành root
+  optimizeDeps: {
+    include: ["scroll-into-view-if-needed"],
+  },
 });
