042f7e212a8cbc1bc1d16f2fc0d135deb3a038fa
DuyMinh2033 <tdminh334546@gmail.com>
2024-11-15 20:57:24 +0700

    test

--- src/Page/InputIOS/index.jsx
+++ src/Page/InputIOS/index.jsx
@@ -5,34 +5,6 @@ const InputIOS = () => {
   const inputRefs = useRef([]);
   const containerRef = useRef(null);
 
-  // useEffect(() => {
-  //   const handleViewportChange = () => {
-  //     console.log("window.visualViewport", window.visualViewport);
-  //     const activeInput = document.activeElement;
-  //     if (inputRefs.current.includes(activeInput)) {
-  //       // activeInput.scrollIntoView({
-  //       //   behavior: "smooth",
-  //       //   block: "center",
-  //       // });
-  //       containerRef.current.scrollTop = window.visualViewport;
-  //     }
-  //   };
-  //   if (window.visualViewport) {
-  //     window.visualViewport.addEventListener("resize", handleViewportChange);
-  //     return () => {
-  //       window.visualViewport.removeEventListener(
-  //         "resize",
-  //         handleViewportChange
-  //       );
-  //     };
-  //   } else {
-  //     window.addEventListener("resize", handleViewportChange);
-  //     return () => {
-  //       window.removeEventListener("resize", handleViewportChange);
-  //     };
-  //   }
-  // }, []);
-
   const debounceChangeOption = (cb, delay) => {
     let timeout;
     return (...args) => {
@@ -42,47 +14,73 @@ const InputIOS = () => {
       }, delay);
     };
   };
-  const handleViewportChange = () => {
-    const activeInput = document.activeElement;
 
-    // Kiểm tra nếu phần tử đang active là một trong những input bạn quan tâm
-    if (inputRefs.current.includes(activeInput)) {
-      // Lấy kích thước của viewport
-      const viewportHeight = window.visualViewport
-        ? window.visualViewport.height
-        : window.innerHeight;
-
-      // Tính toán vị trí cần thiết để field nằm ở giữa viewport
-      const offset = (viewportHeight - 166) / 2;
-      console.log("activeInput.offsetTop", activeInput.offsetTop);
-      // Đảm bảo `containerRef` là phần tử cha chứa tất cả input
-      containerRef.current.scrollTo({
-        top: activeInput.offsetTop - offset,
-        behavior: "smooth",
-      });
-    }
-  };
-
-  const bounce = debounceChangeOption(handleViewportChange, 200);
   useEffect(() => {
-    if (window.visualViewport) {
-      window.visualViewport.addEventListener("resize", bounce);
-    } else {
-      window.addEventListener("resize", bounce);
-    }
-
-    // Cleanup khi unmount
-    return () => {
-      if (window.visualViewport) {
-        window.visualViewport.removeEventListener(
-          "resize",
-          handleViewportChange
-        );
-      } else {
-        window.removeEventListener("resize", handleViewportChange);
+    const handleViewportChange = () => {
+      console.log("window.visualViewport", window.visualViewport);
+      const activeInput = document.activeElement;
+      if (inputRefs.current.includes(activeInput)) {
+        activeInput.scrollIntoView({
+          behavior: "smooth",
+          block: "center",
+        });
       }
     };
+    const bounce = debounceChangeOption(handleViewportChange, 300);
+
+    if (window.visualViewport) {
+      window.visualViewport.addEventListener("resize", bounce);
+      return () => {
+        window.visualViewport.removeEventListener("resize", bounce);
+      };
+    } else {
+      window.addEventListener("resize", bounce);
+      return () => {
+        window.removeEventListener("resize", bounce);
+      };
+    }
   }, []);
+
+  // const handleViewportChange = () => {
+  //   const activeInput = document.activeElement;
+
+  //   // Kiểm tra nếu phần tử đang active là một trong những input bạn quan tâm
+  //   if (inputRefs.current.includes(activeInput)) {
+  //     // Lấy kích thước của viewport
+  //     const viewportHeight = window.visualViewport
+  //       ? window.visualViewport.height
+  //       : window.innerHeight;
+
+  //     // Tính toán vị trí cần thiết để field nằm ở giữa viewport
+  //     const offset = (viewportHeight - 166) / 2;
+  //     console.log("activeInput.offsetTop", activeInput.offsetTop);
+  //     // Đảm bảo `containerRef` là phần tử cha chứa tất cả input
+  //     containerRef.current.scrollTo({
+  //       top: activeInput.offsetTop - offset,
+  //       behavior: "smooth",
+  //     });
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   if (window.visualViewport) {
+  //     window.visualViewport.addEventListener("resize", bounce);
+  //   } else {
+  //     window.addEventListener("resize", bounce);
+  //   }
+
+  //   // Cleanup khi unmount
+  //   return () => {
+  //     if (window.visualViewport) {
+  //       window.visualViewport.removeEventListener(
+  //         "resize",
+  //         handleViewportChange
+  //       );
+  //     } else {
+  //       window.removeEventListener("resize", handleViewportChange);
+  //     }
+  //   };
+  // }, []);
 
   return (
     <div
