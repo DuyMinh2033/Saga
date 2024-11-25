aacee8086f5781b62008b55b80bc6f8bce0902d3
DuyMinh2033 <tdminh334546@gmail.com>
2024-11-25 22:03:03 +0700

    testLLL

--- src/Page/ZoomMobile/index.jsx
+++ src/Page/ZoomMobile/index.jsx
@@ -3,13 +3,34 @@ import "./styles.scss";
 import BottomSheet from "../../components/BottomSheet";
 import { Worker, Viewer } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
-
+import { useGesture } from "react-use-gesture";
 const PinchZoomPDF = () => {
   const [open, setIsOpen] = useState(false);
+  const [scale, setScale] = useState(1);
+  const bind = useGesture(
+    {
+      onPinch: ({ offset: [s], memo = scale }) => {
+        const total = Math.max(0.5, Math.min(4, s * memo));
+        setScale(() => (total > 1 ? total : 1));
+        return memo;
+      },
+    },
+    { pinch: { scaleBounds: { min: 0.5, max: 4 } } }
+  );
+
+  console.log("s", scale);
   return (
     <div className="page__zoom">
       <BottomSheet open={open}>
-        <div style={{ height: "400px", width: "100%" }}>
+        <div
+          {...bind()}
+          style={{
+            touchAction: "none",
+            transform: `scale(${scale})`,
+            height: "400px",
+            width: "100%",
+          }}
+        >
           <Worker
             workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}
           >
