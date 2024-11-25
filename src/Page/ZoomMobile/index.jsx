e0b697e814b56fcfe55244c59504d6c59d450dac
DuyMinh2033 <tdminh334546@gmail.com>
2024-11-25 22:15:01 +0700

    testAndroidStupid

--- src/Page/ZoomMobile/index.jsx
+++ src/Page/ZoomMobile/index.jsx
@@ -1,29 +1,57 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./styles.scss";
 import BottomSheet from "../../components/BottomSheet";
 import { Worker, Viewer } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
-import { useGesture } from "react-use-gesture";
+
 const PinchZoomPDF = () => {
   const [open, setIsOpen] = useState(false);
   const [scale, setScale] = useState(1);
-  const bind = useGesture(
-    {
-      onPinch: ({ offset: [s], memo = scale }) => {
-        const total = Math.max(0.5, Math.min(4, s * memo));
-        setScale(() => (total > 1 ? total : 1));
-        return memo;
-      },
-    },
-    { pinch: { scaleBounds: { min: 0.5, max: 4 } } }
-  );
+  const containerRef = useRef(null);
+  const initialDistance = useRef(0);
 
-  console.log("s", scale);
+  const handleTouchStart = (e) => {
+    if (e.touches.length === 2) {
+      const touch1 = e.touches[0];
+      const touch2 = e.touches[1];
+      initialDistance.current = Math.sqrt(
+        (touch1.pageX - touch2.pageX) ** 2 + (touch1.pageY - touch2.pageY) ** 2
+      );
+    }
+  };
+
+  const handleTouchMove = (e) => {
+    if (e.touches.length === 2) {
+      const touch1 = e.touches[0];
+      const touch2 = e.touches[1];
+      const newDistance = Math.sqrt(
+        (touch1.pageX - touch2.pageX) ** 2 + (touch1.pageY - touch2.pageY) ** 2
+      );
+
+      const scaleChange = newDistance / initialDistance.current;
+      setScale((prevScale) =>
+        Math.max(0.5, Math.min(4, prevScale * scaleChange))
+      );
+      initialDistance.current = newDistance; // cập nhật khoảng cách ban đầu
+    }
+  };
+
+  useEffect(() => {
+    const container = containerRef.current;
+    container.addEventListener("touchstart", handleTouchStart);
+    container.addEventListener("touchmove", handleTouchMove);
+
+    return () => {
+      container.removeEventListener("touchstart", handleTouchStart);
+      container.removeEventListener("touchmove", handleTouchMove);
+    };
+  }, []);
+
   return (
     <div className="page__zoom">
       <BottomSheet open={open}>
         <div
-          {...bind()}
+          ref={containerRef}
           style={{
             touchAction: "none",
             transform: `scale(${scale})`,
