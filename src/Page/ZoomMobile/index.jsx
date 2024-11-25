ff29a56bb4109aebf0d90dccf49ac853940ec1ef
DuyMinh2033 <tdminh334546@gmail.com>
2024-11-25 21:31:53 +0700

    testPDF

--- src/Page/ZoomMobile/index.jsx
+++ src/Page/ZoomMobile/index.jsx
@@ -1,43 +1,27 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import "./styles.scss";
+import BottomSheet from "../../components/BottomSheet";
+import { Worker, Viewer } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+
 const PinchZoomPDF = () => {
-  const [scale, setScale] = useState(1);
-  const containerRef = useRef(null);
-
-  const handleTouchMove = (event) => {
-    if (event.touches.length === 2) {
-      // Tính toán khoảng cách giữa hai ngón tay
-      const touch1 = event.touches[0];
-      const touch2 = event.touches[1];
-      const distance = Math.sqrt(
-        Math.pow(touch2.pageX - touch1.pageX, 2) +
-          Math.pow(touch2.pageY - touch1.pageY, 2)
-      );
-
-      // Lưu trạng thái scale
-      setScale((prevScale) =>
-        Math.min(Math.max(prevScale + distance * 0.001, 1), 3)
-      );
-    }
-  };
-
+  const [open, setIsOpen] = useState(false);
   return (
     <div className="page__zoom">
-      <div
-        ref={containerRef}
-        onTouchMove={handleTouchMove}
-        style={{
-          transform: `scale(${scale})`,
-          transformOrigin: "center",
-          touchAction: "none",
-        }}
-      >
-        <img
-          src="https://cdn.tgdd.vn/Files/2022/03/31/1423175/chuyen-anh-sang-pdf.jpg"
-          alt="PDF Preview"
-          className="img__zom"
-        />
-      </div>
+      <BottomSheet open={open}>
+        <div style={{ height: "400px", width: "100%" }}>
+          <Worker
+            workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}
+          >
+            <Viewer
+              fileUrl={`https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf`}
+            />
+          </Worker>
+        </div>
+      </BottomSheet>
+      <button className="btn" onClick={() => setIsOpen(true)}>
+        Click me
+      </button>
     </div>
   );
 };
