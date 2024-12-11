e348bc47cfd9fd535c9e6cfeddeddab454d1f324
Duy Minh <tdminh334546@gmail.com>
2024-12-11 18:42:32 +0700

    test Input

--- src/Page/Demo/index.jsx
+++ src/Page/Demo/index.jsx
@@ -9,52 +9,8 @@ import Accordion from "../../common/components/Ancordion";
 import ViewMapBottom from "../../common/components/ViewMapBottom";
 
 const Demo = () => {
-  const [limitTransfer, setLimitTransfer] = useState(50);
-  const [step, setStep] = useState();
-  const valueOnchange = useRef("");
-  const result = useRef("");
-  const [isOpen, setIsOpen] = useState(false);
-  const handleSubmit = () => {
-    if (valueOnchange.current === "" || valueOnchange.current === "0") return;
-
-    if (valueOnchange.current > limitTransfer) {
-      result.current = value.increase;
-    } else {
-      result.current = value.decrease;
-    }
-
-    if (result.current) {
-      alert(contents[result.current]);
-    }
-    setStep("completed");
-  };
-
-  const handleTest = () => {
-    const arr = [1, 1, 1, 2, 3, 3, 4];
-    const resultData = [];
-    for (let i = 0; i < arr.length; i++) {
-      const item = arr[i];
-      if (!resultData.includes(item)) {
-        resultData.push(item);
-      }
-    }
-    console.log(resultData);
-  };
-  const refExpand = useRef([]);
-  const [isShow, setIsShow] = useState(null);
-  const timeOut = useRef(null);
-  const handleClick = (index) => {
-    setIsShow(index);
-    clearTimeout(timeOut.current);
-    timeOut.current = setTimeout(() => {
-      if (refExpand.current[index]) {
-        refExpand.current[index].scrollIntoView({
-          behavior: "smooth", // Tạo hiệu ứng mượt khi cuộn
-          block: "center", // Đảm bảo element nằm ở giữa viewport
-        });
-      }
-    }, 100);
-  };
+  const [valueInput, setValeInput] = useState("");
+  const [valueBefor, setValueBefor] = useState("");
 
   const handleOnChange = (e) => {
     console.log(e.target.value);
@@ -62,22 +18,27 @@ const Demo = () => {
 
   const handleOnInput = (e) => {
     const invalidNameRegex = /[^0-9a-zA-Z.,‘’'-\s]/g;
+    setValueBefor(e.target.value);
     if (invalidNameRegex && e.target.value) {
       e.target.value = e.target.value.replace(invalidNameRegex, "");
     }
+    setValeInput(e.target.value);
   };
 
   return (
     <>
-      <button onClick={() => setIsOpen(true)}>click</button>
-      <ViewMapBottom isOpen={isOpen} onClose={() => setIsOpen(false)} />
       <input
         type="text"
         placeholder="street name"
         onChange={handleOnChange}
         onInput={handleOnInput}
+        onCompositionEnd={handleOnInput}
+        onCompositionStart={handleOnInput}
+        onCompositionUpdate={handleOnInput}
       />
       <input type="text" placeholder="city" />
+      <p style={{ color: "InfoText" }}>{valueBefor}</p>
+      <p style={{ color: "red" }}>{valueInput}</p>
     </>
   );
 };
