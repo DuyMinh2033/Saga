cb11a1451a2776255d0efb60e6697479c19c11f3
DuyMinh2033 <tdminh334546@gmail.com>
2024-12-12 00:03:37 +0700

    tetsadasd

--- src/Page/Demo/index.jsx
+++ src/Page/Demo/index.jsx
@@ -36,27 +36,37 @@ const Demo = () => {
   const handleOnChange2 = (e) => {
     setValeInput2(e.target.value);
   };
+  const [key, setKey] = useState(0); // Cấu hình key để ép component render lại
 
+  const handleOnFocus = () => {
+    setKey((prevKey) => prevKey + 1); // Thay đổi key mỗi khi focus vào input
+  };
   return (
     <>
       <input
+        key={key}
         value={valueInput}
         type="text"
         placeholder="street name"
         onChange={handleOnChange}
         onKeyDown={handleKeyDown}
         autoComplete="new-password"
+        name={`input-${Math.random()}`} // Sử dụng tên trường ngẫu nhiên
         autoCorrect="off"
         spellCheck="false"
+        inputMode="text" // Cung cấp hướng dẫn rõ ràng cho trình duyệt
       />
       <input
+        key={key + 2}
         type="text"
         placeholder="city"
         value={valueInput2}
         onChange={handleOnChange2}
         autoComplete="new-password"
+        name={`input-${Math.random()}`} // Sử dụng tên trường ngẫu nhiên
         autoCorrect="off"
         spellCheck="false"
+        inputMode="text" // Cung cấp hướng dẫn rõ ràng cho trình duyệt
       />
       <p style={{ color: "red" }}>input 1 : {valueInput}</p>
       <p style={{ color: "red" }}>input 2: {valueInput2}</p>
