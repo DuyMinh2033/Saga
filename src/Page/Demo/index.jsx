358ae898b9e1448efae3f1fb621d8e4f856cad16
Duy Minh <tdminh334546@gmail.com>
2024-12-12 17:05:04 +0700

    怪十勝富樫千寿千寿千寿千寿

--- src/Page/Demo/index.jsx
+++ src/Page/Demo/index.jsx
@@ -6,84 +6,89 @@ const Demo = () => {
   const [valueInput, setValeInput] = useState("");
   const [valueInput2, setValeInput2] = useState("");
 
-  const invalidTest = /[^0-9a-zA-Z.,‘’'-\s]/g;
-  const handleOnChange = (e) => {
-    let value = e.target.value;
-    if (value && invalidTest.test(value)) {
-      value = value.replace(invalidTest, "");
-    }
-    setValeInput(value);
+  // const invalidTest = /[^0-9a-zA-Z.,‘’'-\s]/g;
+  // const handleOnChange = (e) => {
+  //   let value = e.target.value;
+  //   if (value && invalidTest.test(value)) {
+  //     value = value.replace(invalidTest, "");
+  //   }
+  //   setValeInput(value);
+  // };
+
+  // const handleKeyDown = (event) => {
+  //   const ignoreKeys = [
+  //     "ArrowLeft",
+  //     "ArrowRight",
+  //     "ArrowUp",
+  //     "ArrowDown",
+  //     "Backspace",
+  //     "Delete",
+  //     "Tab",
+  //     "Enter",
+  //     "Escape",
+  //     "Home",
+  //     "End",
+  //     "PageUp",
+  //     "PageDown",
+  //   ];
+  //   if (ignoreKeys.includes(event.key)) {
+  //     return;
+  //   }
+  //   if (event.ctrlKey || event.metaKey) {
+  //     return;
+  //   }
+
+  //   let convertRegex = new RegExp(invalidTest);
+  //   if (convertRegex.test(event.key)) {
+  //     event.preventDefault();
+  //   }
+  // };
+
+  // const handleOnChange2 = (e) => {
+  //   setValeInput2(e.target.value);
+  // };
+  // const [key, setKey] = useState(0);
+
+  // const inputRef = useRef(null);
+  // const [isReadyOnly, setIsReadyOnly] = useState(true);
+
+  // const handleOnFocus = () => {
+  //   setIsReadyOnly(false);
+  // };
+
+  const removeSpecials = (evt) => {
+    const value = evt.target.value;
+    const patt = /[^\x20-\x7E]+/;
+    setValeInput(value.replace(patt, ""));
   };
-
-  const handleKeyDown = (event) => {
-    const ignoreKeys = [
-      "ArrowLeft",
-      "ArrowRight",
-      "ArrowUp",
-      "ArrowDown",
-      "Backspace",
-      "Delete",
-      "Tab",
-      "Enter",
-      "Escape",
-      "Home",
-      "End",
-      "PageUp",
-      "PageDown",
-    ];
-    if (ignoreKeys.includes(event.key)) {
-      return;
-    }
-    if (event.ctrlKey || event.metaKey) {
-      return;
-    }
-
-    let convertRegex = new RegExp(invalidTest);
-    if (convertRegex.test(event.key)) {
-      event.preventDefault();
-    }
-  };
-
-  const handleOnChange2 = (e) => {
-    setValeInput2(e.target.value);
-  };
-  const [key, setKey] = useState(0);
-
-  const inputRef = useRef(null);
-  const [isReadyOnly, setIsReadyOnly] = useState(true);
-
-  const handleOnFocus = () => {
-    setIsReadyOnly(false);
-  };
-
   return (
     <form autoComplete="new-password" method="" action="">
       <input
-        key={key}
+        // key={key}
         value={valueInput}
         type="text"
         placeholder="street name"
-        onChange={handleOnChange}
-        onKeyDown={handleKeyDown}
+        onChange={removeSpecials}
+        // onKeyDown={handleKeyDown}
         autoComplete="new-password"
         name={`input-${Math.random()}`}
         autoCorrect="off"
         spellCheck="false"
       />
       <input
-        ref={inputRef}
-        key={key + 2}
-        type="number"
-        placeholder="city"
-        value={valueInput2}
-        onChange={handleOnChange2}
-        autoComplete="new-password"
-        name={`input-${Math.random()}`}
-        autoCorrect="off"
-        spellCheck="false"
-        inputMode="text"
-        onFocus={handleOnFocus}
-        readOnly={isReadyOnly}
+      // ref={inputRef}
+      // key={key + 2}
+      // type="number"
+      // placeholder="city"
+      // value={valueInput2}
+      // onChange={handleOnChange2}
+      // autoComplete="new-password"
+      // name={`input-${Math.random()}`}
+      // autoCorrect="off"
+      // spellCheck="false"
+      // inputMode="text"
+      // onFocus={handleOnFocus}
+      // readOnly={isReadyOnly}
       />
       <p style={{ color: "red" }}>input 1 : {valueInput}</p>
       <p style={{ color: "red" }}>input 2: {valueInput2}</p>
