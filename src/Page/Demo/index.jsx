c2b8a11e84353444c4edbdb0951300b71e670fda
DuyMinh2033 <tdminh334546@gmail.com>
2024-12-12 21:45:55 +0700

    kaka

--- src/Page/Demo/index.jsx
+++ src/Page/Demo/index.jsx
@@ -1,68 +1,22 @@
-/* eslint-disable no-unused-vars */
-import { Fragment, useRef, useState } from "react";
-import "./styles.scss";
+import { useState } from "react";
 
 const Demo = () => {
-  const [valueInput, setValeInput] = useState("");
-  const [valueInput2, setValeInput2] = useState("");
+  const [valueInput, setValueInput] = useState("");
+  console.log("🚀 ~ Demo ~ valueInput:", valueInput);
+  const [valueInput2, setValueInput2] = useState("");
 
-  const invalidTest = /[^0-9a-zA-Z.,‘’'-\s]/g;
-  // const handleOnChange = (e) => {
-  //   let value = e.target.value;
-  //   if (value && invalidTest.test(value)) {
-  //     value = value.replace(invalidTest, "");
-  //   }
-  //   setValeInput(value);
-  // };
-
-  const handleKeyDown = (event) => {
-    const ignoreKeys = [
-      "ArrowLeft",
-      "ArrowRight",
-      "ArrowUp",
-      "ArrowDown",
-      "Backspace",
-      "Delete",
-      "Tab",
-      "Enter",
-      "Escape",
-      "Home",
-      "End",
-      "PageUp",
-      "PageDown",
-    ];
-    if (ignoreKeys.includes(event.key)) {
-      return;
+  const handleOnChange1 = (e) => {
+    const invalidTest = /[^0-9a-zA-Z.,‘’'-\s]/g;
+    const isChecked = invalidTest.test(e.target.value);
+    if (isChecked) {
+      return null;
     }
-    if (event.ctrlKey || event.metaKey) {
-      return;
-    }
-    let convertRegex = new RegExp(/[^\x20-\x7E]+/);
-    if (convertRegex.test(event.key)) {
-      event.preventDefault();
-    }
+    setValueInput(e.target.value);
   };
-
-  // const [key, setKey] = useState(0);
-
-  // const inputRef = useRef(null);
-  // const [isReadyOnly, setIsReadyOnly] = useState(true);
-
-  // const handleOnFocus = () => {
-  //   setIsReadyOnly(false);
-  // };
 
   const handleOnChange2 = (e) => {
-    console.log("value2", e.target.value);
-    setValeInput2(e.target.value);
+    setValueInput2(e.target.value);
   };
-  const removeSpecials = (evt) => {
-    const value = evt.target.value;
-    const patt = /[^\x20-\x7E]+/;
-    setValeInput(value.replace(patt, ""));
-  };
-
-  const inputs = Array.from({ length: 20 }, (_, index) => index + 1);
 
   return (
     <form
@@ -81,37 +35,23 @@ const Demo = () => {
         value={valueInput}
         type="text"
         placeholder="street name"
-        onChange={removeSpecials}
-        onKeyDown={(e) => e.preventDefault()}
-        style={{ imeMode: "disabled" }} // Prevents IME on some browsers
-        inputMode="text"
-        autoComplete="off"
-        autoCorrect="off"
-        spellCheck="false"
+        onChange={handleOnChange1}
+        // onKeyDown={(e) => e.preventDefault()}
+        // inputMode="text"
+        // autoComplete="new-password"
+        // autoCorrect="off"
+        // spellCheck="false"
       />
       <input
         value={valueInput2}
+        // onFocus={handleFocus2} // Xóa cache khi focus
         onChange={handleOnChange2}
         placeholder="street"
-        inputMode="text"
-        autoComplete="off"
-        autoCorrect="off"
-        spellCheck="false"
-        // onCompositionEnd={(e) => handleCompositionEnd(e)}
+        // inputMode="text"
+        // autoComplete="new-password"
+        // autoCorrect="off"
+        // spellCheck="false"
       />
-      {inputs.map((input, index) => (
-        <Fragment key={index}>
-          <input
-            type="text"
-            id={`input-${index}`}
-            name={`input-${index}`}
-            inputMode="text"
-            autoComplete="off"
-            autoCorrect="off"
-            spellCheck="false"
-          />
-        </Fragment>
-      ))}
       <p style={{ color: "red" }}>input 1 : {valueInput}</p>
       <p style={{ color: "red" }}>input 2: {valueInput2}</p>
     </form>
