2d9f96103d091e74c2d1a09cb8c5b39f6e1ea687
Duy Minh <tdminh334546@gmail.com>
2024-12-13 09:48:47 +0700

    JA

--- src/Page/Demo/index.jsx
+++ src/Page/Demo/index.jsx
@@ -1,21 +1,64 @@
-import { useState } from "react";
+/* eslint-disable no-unused-vars */
+import { Fragment, useRef, useState } from "react";
+import "./styles.scss";
 
 const Demo = () => {
-  const [valueInput, setValueInput] = useState("");
-  console.log("🚀 ~ Demo ~ valueInput:", valueInput);
-  const [valueInput2, setValueInput2] = useState("");
+  const [valueInput, setValeInput] = useState("");
+  const [valueInput2, setValeInput2] = useState("");
 
-  const handleOnChange1 = (e) => {
-    const invalidTest = /[^0-9a-zA-Z.,‘’'-\s]/g;
-    const isChecked = invalidTest.test(e.target.value);
-    if (isChecked) {
-      return null;
-    }
-    setValueInput(e.target.value);
-  };
+  const invalidTest = /[^0-9a-zA-Z.,‘’'-\s]/g;
+
+  // const handleOnChange = (e) => {
+  //   let value = e.target.value;
+  //   if (value && invalidTest.test(value)) {
+  //     value = value.replace(invalidTest, "");
+  //   }
+  //   setValeInput(value);
+  // };
+
+  // const handleKeyDown = (event) => {
+  //   const ignoreKeys = [
+  //     "ArrowLeft",
+  //     "ArrowRight",
+  //     "ArrowUp",
+  //     "ArrowDown",
+  //     "Backspace",
+  //     "Delete",
+  //     "Tab",
+  //     "Enter",
+  //     "Escape",
+  //     "Home",
+  //     "End",
+  //     "PageUp",
+  //     "PageDown",
+  //   ];
+  //   if (ignoreKeys.includes(event.key)) {
+  //     return;
+  //   }
+  //   if (event.ctrlKey || event.metaKey) {
+  //     return;
+  //   }
+  //   let convertRegex = new RegExp(/[^\x20-\x7E]+/);
+  //   if (convertRegex.test(event.key)) {
+  //     event.preventDefault();
+  //   }
+  // };
 
   const handleOnChange2 = (e) => {
-    setValueInput2(e.target.value);
+    console.log("value2", e.target.value);
+    setValeInput2(e.target.value);
+  };
+
+  const handleCompositionEnd = (e) => {
+    console.log({
+      valuehandleCompositionEnd: e.target.value,
+      default: valueInput2,
+    });
+
+    if (e.target.value !== valueInput2) {
+      e.target.value = valueInput2;
+      setValeInput2(valueInput2);
+    }
   };
 
   return (
@@ -35,23 +78,24 @@ const Demo = () => {
         value={valueInput}
         type="text"
         placeholder="street name"
-        onChange={handleOnChange1}
-        // onKeyDown={(e) => e.preventDefault()}
-        // inputMode="text"
-        // autoComplete="new-password"
-        // autoCorrect="off"
-        // spellCheck="false"
+        onKeyDown={(e) => e.preventDefault()}
+        style={{ imeMode: "disabled" }} // Prevents IME on some browsers
+        inputMode="text"
+        autoComplete="off"
+        autoCorrect="off"
+        spellCheck="false"
       />
       <input
         value={valueInput2}
-        // onFocus={handleFocus2} // Xóa cache khi focus
         onChange={handleOnChange2}
         placeholder="street"
-        // inputMode="text"
-        // autoComplete="new-password"
-        // autoCorrect="off"
-        // spellCheck="false"
+        inputMode="text"
+        autoComplete="off"
+        autoCorrect="off"
+        spellCheck="false"
+        onCompositionEnd={(e) => handleCompositionEnd(e)}
       />
+
       <p style={{ color: "red" }}>input 1 : {valueInput}</p>
       <p style={{ color: "red" }}>input 2: {valueInput2}</p>
     </form>
