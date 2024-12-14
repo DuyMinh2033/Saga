39f03559c2c43d98b8c125c0b9cfc5f3c6b536b5
DuyMinh2033 <tdminh334546@gmail.com>
2024-12-14 08:39:17 +0700

    tesssatsdtastd

--- src/Page/Demo/index.jsx
+++ src/Page/Demo/index.jsx
@@ -1,24 +1,33 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./styles.scss";
 
 const Demo = () => {
-  const [valueInput2, setValueInput2] = useState("");
-  const [isComposition, setIsComposition] = useState(false);
+  const [valueInput2, setValeInput2] = useState("");
 
+  const isFirstFocus = useRef(true);
+
+  const [isComposition, setIsComposition] = useState(false);
+  const [isEnter, setIsEnter] = useState(false);
   const handleOnChange2 = (e) => {
     const value = e.target.value;
-    if (!isComposition) {
-      setValueInput2(value);
+    if (!isComposition || isEnter) {
+      setValeInput2(value);
     }
   };
 
-  const handleCompositionStart = () => {
-    setIsComposition(true);
+  const handleCompositionEnd = (e) => {
+    console.log("e :>>");
+    setIsComposition(false);
+    setValeInput2(e.target.value);
+    e.target.blur();
   };
 
-  const handleCompositionEnd = (e) => {
-    setIsComposition(false);
-    setValueInput2(e.target.value);
+  const handleCompositionStart = () => {
+    if (isFirstFocus.current) {
+      setIsComposition(true);
+      isFirstFocus.current = false;
+      console.log("First focus: set isComposition true");
+    }
   };
 
   return (
@@ -36,15 +45,18 @@ const Demo = () => {
         placeholder="street name"
         onKeyDown={(e) => e.preventDefault()}
       />
-
       <input
         value={valueInput2}
         onChange={handleOnChange2}
         placeholder="street"
         onCompositionStart={handleCompositionStart}
         onCompositionEnd={handleCompositionEnd}
+        onKeyDown={() => setIsEnter(true)}
+        onBlur={() => {
+          isFirstFocus.current = true;
+        }}
       />
-
+      {/* <p style={{ color: "red" }}>input 1 : {valueInput}</p> */}
       <p style={{ color: "red" }}>input 2: {valueInput2}</p>
     </form>
   );
