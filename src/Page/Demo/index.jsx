f6a31d58faf9182d6aba3c1afd675908549a0658
DuyMinh2033 <tdminh334546@gmail.com>
2024-12-17 22:28:34 +0700

    test input

--- src/Page/Demo/index.jsx
+++ src/Page/Demo/index.jsx
@@ -1,34 +1,43 @@
-import { useEffect, useState, useTransition } from "react";
 import "./styles.scss";
+import Input from "../../components/Input";
+import { Controller, useForm } from "react-hook-form";
+
+const regexInput = /[^0-9a-zA-Z.,‘’'-\s]/g;
 
 const Demo = () => {
-  const [isPending, startTransition] = useTransition();
-  const [data, setData] = useState([]);
+  const { control, handleSubmit } = useForm();
 
-  useEffect(() => {
-    startTransition(async () => {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
-      const result = await response.json();
-      startTransition(() => {
-        setData(result);
-      });
-    });
-  }, []);
+  const submitForm = (value) => {
+    alert(JSON.stringify(value, null, 2));
+  };
 
   return (
-    <div className="over">
-      {/* {isPending && <p>Updating UI...</p>}
-      <ul>{data && data.map((item) => <li key={item.id}>{item.title}</li>)}</ul> */}
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "20px",
+          height: "80vh",
+          overflow: "auto",
+        }}
+      >
+        <Controller
+          name="email"
+          control={control}
+          render={({ field }) => <Input regex={regexInput} {...field} />}
+        />
 
-      <div className="test_ui">
-        <div className="test_container">
-          <p>Hello world!</p>
-          <p>Hello everyone!</p>
-        </div>
+        <Controller
+          name="streetName"
+          control={control}
+          render={({ field }) => <Input regex={regexInput} {...field} />}
+        />
+        <button onClick={handleSubmit(submitForm)}>Submit</button>
       </div>
-    </div>
+    </>
   );
 };
 
