6295392744cbe286ecf3458bcd19afafaf0a1b05
DuyMinh2033 <tdminh334546@gmail.com>
2025-03-16 10:23:14 +0700

    add 0 to the first digit

--- src/Page/Demo/contanst.ts
+++ src/Page/Demo/contanst.ts
@@ -10,6 +10,7 @@ export const formatBirthDay = (value: string, prevValue: string = '') => {
   if (!value) return '';
   const isBackSpace = value.length < prevValue.length;
   let result = value.replace(/\D/g, '');
+  console.log('🚀 ~ formatBirthDay ~ result:', typeof result);
 
   if (isBackSpace) {
     if (prevValue.endsWith(' ') && value.length === prevValue.length - 1) {
@@ -18,30 +19,32 @@ export const formatBirthDay = (value: string, prevValue: string = '') => {
   }
 
   if (result.length === 1) {
-    const firstMonth = Number(result[0]);
-    if (firstMonth !== 0 && firstMonth !== 1) {
+    const firstDigit = Number(result[0]);
+    if (![0, 1].includes(firstDigit) && firstDigit < 10 && !isBackSpace) {
+      return (result = `0${firstDigit} `); // add 0 to the first digit
+    } else if (![0, 1].includes(firstDigit)) {
       return prevValue;
     }
   }
 
   if (result.length === 3) {
-    const firstDay = Number(result[2]);
-    if (firstDay !== 0 && firstDay !== 1 && firstDay !== 2 && firstDay !== 3) {
+    const threeDigits = Number(result[2]);
+    if (![0, 1, 2, 3].includes(threeDigits)) {
       return prevValue;
     }
   }
 
   if (result.length >= 2) {
-    let month = result.slice(0, 2);
-    if (parseInt(month, 10) < 1 || parseInt(month, 10) > 12) {
+    let month = Number(result.slice(0, 2));
+    if (month < 1 || month > 12) {
       return prevValue;
     }
     result = result.slice(0, 2) + ' ' + result.slice(2);
   }
 
   if (result.length >= 5) {
-    const day = result.slice(3, 5);
-    if (parseInt(day, 10) < 1 || parseInt(day, 10) > 31) {
+    const day = Number(result.slice(3, 5));
+    if (day < 1 || day > 31) {
       return prevValue;
     }
     result = result.slice(0, 5) + ' ' + result.slice(5, 9);
