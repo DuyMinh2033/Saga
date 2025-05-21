133862e2c6707adb45f32ce01a41ef7d7d460301
Duy Minh <tdminh334546@gmail.com>
2025-05-21 16:57:42 +0700

    test

--- src/Page/Demo/index.tsx
+++ src/Page/Demo/index.tsx
@@ -1,44 +1,30 @@
-import './styles.scss';
-import { Controller, useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { useEffect } from 'react';
+/* eslint-disable no-undef */
+import { useRef } from 'react';
 
-import { dobSchema, formatBirthDay } from './contanst';
+function Demo() {
+  const videoRef = useRef(null);
 
-const Demo = () => {
-  const {
-    control,
-    formState: { errors },
-  } = useForm({
-    mode: 'onChange',
-    resolver: yupResolver(dobSchema),
-    defaultValues: { test: '' },
-  });
-
-  useEffect(() => {
-    console.log(formatBirthDay('02292005'));
-  }, []);
+  const openCamera = async () => {
+    if (typeof navigator !== 'undefined' && navigator.mediaDevices) {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      } catch (err) {
+        alert('Không thể mở camera: ' + err?.message);
+      }
+    } else {
+      alert('Trình duyệt không hỗ trợ hoặc không thể truy cập camera');
+    }
+  };
 
   return (
-    <div className="w-full h-[100vh] flex justify-center items-center flex-col">
-      <div className="px-[24px] w-full">
-        <Controller
-          control={control}
-          name="test"
-          render={({ field: { onChange, value = '' } }) => (
-            <input
-              type="text"
-              value={value}
-              onChange={(e) => {
-                onChange(formatBirthDay(e.target.value, value));
-              }}
-            />
-          )}
-        />
-        {errors?.test && <p style={{ color: 'red', fontSize: '13px' }}>{errors.test.message}</p>}
-      </div>
+    <div>
+      <button onClick={openCamera}>Mở Camera</button>
+      <video ref={videoRef} autoPlay style={{ width: 400, height: 300 }} />
     </div>
   );
-};
+}
 
 export default Demo;
