30e2128529491afd077ff47a8400297243ada3bd
DuyMinh2033 <tdminh334546@gmail.com>
2025-10-16 22:23:03 +0700

    tét

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,20 +1,20 @@
 /* eslint-disable no-undef */
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 
 const Home = () => {
   const ref = useRef<HTMLInputElement>(null);
   const buttonRef = useRef<HTMLButtonElement>(null);
 
-  useEffect(() => {
-    const timeOut = setTimeout(() => {
-      buttonRef.current?.click();
-    }, 3000);
-    return () => clearTimeout(timeOut);
-  }, []);
-
   return (
     <div className="h-screen w-screen flex justify-center items-center">
-      <input type="text" inputMode="numeric" ref={ref} autoComplete="off" autoCapitalize="off" />
+      <input
+        type="text"
+        inputMode="numeric"
+        ref={ref}
+        autoComplete="off"
+        autoCapitalize="off"
+        autoFocus
+      />
 
       <button ref={buttonRef} onClick={() => ref.current?.focus()}>
         click to focus
